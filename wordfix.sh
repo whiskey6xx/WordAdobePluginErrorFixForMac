27184822262f58c6996dc4a134aeea641f51bfe6
whiskey6xx <87744468+whiskey6xx@users.noreply.github.com>
2023-05-02 14:15:39 -0400

    wordfix.sh
    added script

---
+++ wordfix.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -f "/Users/$USER/Library/Group Containers/UBF8T346G9.Office/User Content.localized/Startup.localized/Word/linkCreation.dotm"
+rm -f "/Users/$USER/Library/Group Containers/UBF8T346G9.Office/User Content.localized/Startup.localized/Excel/linkCreation.dotm"
+rm -f "/Users/$USER/Library/Group Containers/UBF8T346G9.Office/User Content.localized/Startup.localized/Powerpoint/linkCreation.dotm"
+rm -f /Library/Application Support/Adobe/MACPDFM
+
+#use chmod +x on this file before use
+#run as root or with sudo
